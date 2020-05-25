4422668c2ceebbd9117e9eb347b6a298b04950ec
GrigorKhachatryan <grigor777001@yandex.ru>
2020-05-25 13:48:53 +0300

    rabbit

--- web/tasks.py
+++ web/tasks.py
@@ -1,13 +1,4 @@
 from user_data import Information
-import psycopg2.extras
-from settings import PASSWORD, DATABASE, USER
-
-connection = psycopg2.connect(user=USER,
-                              password=PASSWORD,
-                              host="ec2-52-72-65-76.compute-1.amazonaws.com",
-                              port="5432",
-                              database=DATABASE)
-cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
 def insta_tasks(login):
