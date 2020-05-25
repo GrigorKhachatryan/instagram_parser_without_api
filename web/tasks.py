071e6d96bcb8ec809756073c469802b816299b9a
GrigorKhachatryan <grigor777001@yandex.ru>
2020-05-25 16:27:39 +0300

    create worker

--- web/tasks.py
+++ web/tasks.py
@@ -2,8 +2,6 @@ from user_data import Information
 
 
 def insta_tasks(login):
-    # obj = Information(nickname=login)
-    # country_code = obj.similar_users()
-    print(0)
-    country_code = ['aa','vv','cc']
+    obj = Information(nickname=login)
+    country_code = obj.similar_users()
     return country_code
