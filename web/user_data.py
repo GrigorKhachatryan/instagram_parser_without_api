5e7a5d99e7cb85bad52d1848459871f27f4dfb87
GrigorKhachatryan <grigor777001@yandex.ru>
2020-05-25 16:52:38 +0300

    create h5 web

--- web/user_data.py
+++ web/user_data.py
@@ -29,7 +29,7 @@ class Information():
         self.url = f'https://www.instagram.com/{nickname}/?__a=1'
         self.love_list = {'result':{}}
         self.model_lstm = Model().lstm()
-        self.model_lstm.load_weights('/web/best_model_lstm.h5')
+        self.model_lstm.load_weights('../web/best_model_lstm.h5')
 
         with open('../analysis/sentiment_analysis/tokenizer.pickle', 'rb') as handle:
             self.tokenizer = pickle.load(handle)
